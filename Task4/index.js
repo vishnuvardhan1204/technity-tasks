2e63fcde8023aaada77211dfddcd2248aaa65cad
vishnuvardhan1204 <124138567+vishnuvardhan1204@users.noreply.github.com>
2023-09-12 16:01:36 +0530

    Add files via upload

---
+++ Task4/index.js
@@ -0,0 +1,28 @@
+function play() {
+    var wbutton = document.getElementById("wbutton");
+    wbutton.play();
+}
+function play2() {
+    var abutton = document.getElementById("abutton");
+    abutton.play();
+}
+function play3() {
+    var sbutton = document.getElementById("sbutton");
+    sbutton.play();
+}
+function play4() {
+    var dbutton = document.getElementById("dbutton");
+    dbutton.play();
+}
+function play5() {
+    var jbutton = document.getElementById("jbutton");
+    jbutton.play();
+}
+function play6() {
+    var kbutton = document.getElementById("kbutton");
+    kbutton.play();
+}
+function play7() {
+    var lbutton = document.getElementById("lbutton");
+    lbutton.play();
+}
